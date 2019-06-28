89e2f79c59fd2ec664f77d8c25544781505eea4d
yueking <yuekinger@gmail.com>
2019-06-28 17:11:09 +0800

    sys dict 联合主键oky

--- src/test/java/com/yueking/core/dao/SysDictDaoTest.java
+++ src/test/java/com/yueking/core/dao/SysDictDaoTest.java
@@ -16,34 +16,39 @@ public class SysDictDaoTest extends BaseTest {
     @Autowired
     SysDictDao dictDao;
 
-    @Test
+    @Test(dependsOnMethods = "testRemove")
     public void testAddDict() {
-//        SysDictKey key = new SysDictKey();
-//        key.setDictType("XB");
-//        key.setDictKey("XB");
-//        SysDict dictRoot = new SysDict();
-//        dictRoot.setId(key);
-//        dictRoot.setDictValue("value");
-//        dictRoot.setRoot(true);
-//        dictRoot.setLevel(0);
-//
-//        SysDict d1 = new SysDict();
-//        d1.setId(new SysDictKey("1","XB"));
-//        d1.setDictValue("男");
-//
-//        SysDict d2 = new SysDict();
-//        d2.setId(new SysDictKey("2","XB"));
-//        d2.setDictValue("女");
-//
-//        Set list = new HashSet();
-//        list.add(d1);
-//        list.add(d2);
-//
-//        dictRoot.setSubDictList(list);
-//
-//
-//
-//
-//        dictDao.save(dictRoot);
+        SysDictKey key = new SysDictKey();
+        key.setDictType("XB");
+        key.setDictKey("XB");
+        SysDict dictRoot = new SysDict();
+        dictRoot.setId(key);
+        dictRoot.setDictValue("value");
+        dictRoot.setRoot(true);
+        dictRoot.setLevel(0);
+
+        SysDict d1 = new SysDict();
+        d1.setId(new SysDictKey("1","XB"));
+        d1.setDictValue("男");
+
+        SysDict d2 = new SysDict();
+        d2.setId(new SysDictKey("2","XB"));
+        d2.setDictValue("女");
+
+        Set list = new HashSet();
+        list.add(d1);
+        list.add(d2);
+
+        dictRoot.setSubDictList(list);
+
+
+
+
+        dictDao.save(dictRoot);
+    }
+
+    @Test
+    public void testRemove() {
+        dictDao.deleteAll();
     }
 }
