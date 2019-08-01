1d2f6d5bd1b1360842bb8387320d80ee3822344e
yueking <yuekinger@gmail.com>
2019-08-01 19:31:44 +0800

    静态资源放行

--- src/main/java/com/yueking/core/conf/WebConfig.java
+++ src/main/java/com/yueking/core/conf/WebConfig.java
@@ -16,6 +16,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.servlet.ServletException;
@@ -28,6 +30,16 @@ import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
+    /**
+     * 静态资源放行
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //静态资源放行
+//        registry.addResourceHandler("/view/**").addResourceLocations("classpath:/view/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
     /**
      * 修改自定义消息转换器
      * @param converters
